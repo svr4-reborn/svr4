c9dafe9ec2762618abf2f1778ec77d04f7e296f9
Alexander Richards <electrodeyt@gmail.com>
2026-06-02 22:02:07 +0200

    extras/init: init loopback network device

--- extras/init/init.c
+++ extras/init/init.c
@@ -1,10 +1,17 @@
+#ifndef _DEFAULT_SOURCE
+#define _DEFAULT_SOURCE
+#endif
+
 #include <fcntl.h>
+#include <net/if.h>
+#include <netinet/in.h>
 #include <stdio.h>
 #include <sys/stat.h>
 #include <sys/sysmacros.h>
 #include <sys/types.h>
 #include <sys/ioctl.h>
 #include <stropts.h>
+#include <string.h>
 #include <termios.h>
 #include <unistd.h>
 #include <stdlib.h>
@@ -15,6 +22,14 @@
 #define FMNAMESZ 8
 #define MAXAPUSH 8
 #define GVID_SETTABLE ((('G' << 8) | 'v') << 16 | 1)
+#define SVR4_IOCPARM_MASK 0xffUL
+#define SVR4_IOC_IN 0x80000000UL
+#define SVR4_IOW(group, number, type) \
+    (SVR4_IOC_IN | ((sizeof(type) & SVR4_IOCPARM_MASK) << 16) | ((group) << 8) | (number))
+#define SVR4_SIOCSIFADDR SVR4_IOW('i', 12, struct ifreq)
+#define SVR4_SIOCSIFFLAGS SVR4_IOW('i', 16, struct ifreq)
+#define SVR4_SIOCSIFNETMASK SVR4_IOW('i', 26, struct ifreq)
+#define SVR4_SIOCSIFNAME SVR4_IOW('i', 73, struct ifreq)
 
 typedef unsigned int major_t;
 
@@ -151,6 +166,110 @@ static void initialize_workstation_console(void) {
     workstation_initialized = 1;
 }
 
+static unsigned long ipv4_address(unsigned int first, unsigned int second,
+        unsigned int third, unsigned int fourth) {
+    return ((fourth & 0xff) << 24) | ((third & 0xff) << 16)
+        | ((second & 0xff) << 8) | (first & 0xff);
+}
+
+static int stream_ioctl(int fd, int command, void *buffer, int length) {
+    struct strioctl request;
+
+    memset(&request, 0, sizeof(request));
+    request.ic_cmd = command;
+    request.ic_timout = 0;
+    request.ic_len = length;
+    request.ic_dp = buffer;
+    return ioctl(fd, I_STR, &request);
+}
+
+static int name_loopback_provider(int ipfd, int linkid) {
+    struct ifreq request;
+
+    memset(&request, 0, sizeof(request));
+    snprintf(request.ifr_name, sizeof(request.ifr_name), "lo0");
+    request.ifr_metric = linkid;
+    return stream_ioctl(ipfd, SVR4_SIOCSIFNAME, &request, sizeof(request));
+}
+
+static void fill_ipv4_request(struct ifreq *request, unsigned long address) {
+    struct sockaddr_in *sin;
+
+    memset(request, 0, sizeof(*request));
+    snprintf(request->ifr_name, sizeof(request->ifr_name), "lo0");
+    sin = (struct sockaddr_in *)&request->ifr_addr;
+    sin->sin_family = AF_INET;
+    sin->sin_addr.s_addr = address;
+}
+
+static int configure_loopback_provider(int ipfd) {
+    struct ifreq request;
+
+    memset(&request, 0, sizeof(request));
+    snprintf(request.ifr_name, sizeof(request.ifr_name), "lo0");
+    request.ifr_flags = IFF_UP | IFF_LOOPBACK | IFF_RUNNING;
+    if(stream_ioctl(ipfd, SVR4_SIOCSIFFLAGS, &request, sizeof(request)) < 0)
+        return -1;
+
+    fill_ipv4_request(&request, ipv4_address(255, 0, 0, 0));
+    if(stream_ioctl(ipfd, SVR4_SIOCSIFNETMASK, &request, sizeof(request)) < 0)
+        return -1;
+
+    fill_ipv4_request(&request, ipv4_address(127, 0, 0, 1));
+    return stream_ioctl(ipfd, SVR4_SIOCSIFADDR, &request, sizeof(request));
+}
+
+static void link_protocol_to_ip(const char *protocol_path) {
+    int protocolfd;
+    int ipfd;
+
+    protocolfd = open(protocol_path, O_RDWR);
+    if(protocolfd < 0)
+        return;
+
+    ipfd = open("/dev/ip", O_RDWR);
+    if(ipfd >= 0) {
+        ioctl(protocolfd, I_PLINK, ipfd);
+        close(ipfd);
+    }
+    close(protocolfd);
+}
+
+static void initialize_network_loopback(void) {
+    static const char *const protocol_paths[] = {
+        "/dev/tcp",
+        "/dev/udp",
+        "/dev/rawip",
+        "/dev/icmp"
+    };
+    unsigned int i;
+    int ipfd;
+    int loopfd;
+    int linkid;
+
+    ipfd = open("/dev/ip", O_RDWR);
+    if(ipfd < 0)
+        return;
+
+    loopfd = open("/dev/loop", O_RDWR);
+    if(loopfd < 0) {
+        close(ipfd);
+        return;
+    }
+
+    linkid = ioctl(ipfd, I_PLINK, loopfd);
+    if(linkid >= 0 && name_loopback_provider(ipfd, linkid) == 0) {
+        if(configure_loopback_provider(ipfd) == 0) {
+            for(i = 0; i < sizeof(protocol_paths) / sizeof(protocol_paths[0]); ++i) {
+                link_protocol_to_ip(protocol_paths[i]);
+            }
+        }
+    }
+
+    close(loopfd);
+    close(ipfd);
+}
+
 static void configure_console(int fd) {
     struct termios modes;
 
@@ -222,6 +341,7 @@ int main(int argc, char **argv) {
 
     configure_console_autopush();
     initialize_workstation_console();
+    initialize_network_loopback();
     init_stdio();
     printf("The system is coming up.\r\n");
     fflush(stdout);
